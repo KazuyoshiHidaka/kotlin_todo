f36085ccf6084e17b90c45d0eb838ed8137bebbf
KazuyoshiHidaka <kazuyoshi.first@gmail.com>
2020-03-01 16:44:04 +0900

    NewPageActivityとPageDetailActivityを新しく生成

---
+++ app/src/main/java/hidaka/kotlinstudy/todo/PageDetailActivity.kt
@@ -0,0 +1,12 @@
+package hidaka.kotlinstudy.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PageDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_detail)
+    }
+}
